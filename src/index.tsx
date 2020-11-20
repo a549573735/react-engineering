35be49ec785feb2bc245fe1b7be92ddc19870cd6
李翔 <a549573735@gmail.com>
2020-11-20 15:46:08 +0800

    fix: update xxx

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,7 @@ import * as ReactDOM from 'react-dom';
 let root = document.getElementById('root');
 
 let props = { className: 'title' };
-let element= React.createElement('div', props, 'hello');
+let element= React.createElement('div', props, 'hello123');
+
+
 ReactDOM.render(element, root);
